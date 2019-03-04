787b7bc7bcf6e62159c70b347540f22d739a7a5b
greentea1120 <wangyukun1120@gmail.com>
2019-03-05 01:14:05 +0800

    index

--- src/components/pages/ShoppingMall.vue
+++ src/components/pages/ShoppingMall.vue
@@ -16,37 +16,93 @@
     <div class="swiper-area">
       <van-swipe :autoplay="1000">
         <van-swipe-item v-for="(banner,index) in bannerPicArray" :key="index">
-          <img v-lazy="banner.imageUrl" width="100%"/>
+          <img v-lazy="banner.image" width="100%"/>
         </van-swipe-item>
       </van-swipe>
     </div>
+    <div class="type-bar">
+      <div  v-for="(cate,index) in category" :key="index" >
+        <img v-lazy="cate.image" width="90%" />
+        <span>{{cate.mallCategoryName}}</span>
+      </div>
+    </div>
+    <div class="ad-banner">
+      <img v-lazy="adBanner.PICTURE_ADDRESS" width="100%">
+    </div>
+    <div class="recommend-area">
+      <div class="recommend-title">
+          商品推荐
+      </div>
+      <div class="recommend-body">
+      </div>
+    </div>
+    <swiper :options="swiperOption">
+      <swiper-slide v-for=" (item ,index) in recommendGoods" :key="index">
+        <div class="recommend-item">
+          <img :src="item.image" width="80%" />
+          <div>{{item.goodsName}}</div>
+          <div>￥{{item.price | moneyFilter}} (￥{{item.mallPrice | moneyFilter}})</div>
+        </div>
+      </swiper-slide>
+    </swiper>
+    <floorComponent :floorData="floor1"></floorComponent>
+    <!-- <SwiperDefault /> -->
+    <!-- <SwiperDefault2 /> -->
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+/* eslint-disable */
+import 'swiper/dist/css/swiper.css'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+import floorComponent from '../component/floorComponent'
+import {toMoney} from '@/filter/moneyFilter.js'
+// import SwiperDefault from '../swiper/swiperDefault'
+// import SwiperDefault2 from '../swiper/swiperDefault2'
 import axios from 'axios'
 export default {
   created() {
     axios({
       url: 'https://www.easy-mock.com/mock/5c7bfb7e852a035c430117a1/mall/index',
       method: 'get'
-    }).then(res => {
-      console.log(res)
+    }).then(response => {
+      console.log(response)
+      if(response.status == 200) {
+        this.category=response.data.data.category
+        this.adBanner = response.data.data.advertesPicture //获得广告图片
+        this.bannerPicArray = response.data.data.slides   //轮播图片
+        this.recommendGoods = response.data.data.recommend  //推荐商品
+        this.floor1 = response.data.data.floor1              //楼层1数据
+      }
+    }).catch((error) => {
     })
+  },
+  filters: {
+    moneyFilter(money){
+      return toMoney(money)
+    }  
   },
   data() {
     return {
+      swiperOption: {
+        slidesPerView: 3
+      },
       msg: 'Shopping Mall',
       locationIcon: require('../../assets/images/location.png'),
       bannerPicArray: [
         { imageUrl: 'https://miniyx.oss-cn-beijing.aliyuncs.com/data/upload/content/20190124/5c4967c876404.gif' },
         { imageUrl: 'https://miniyx.oss-cn-beijing.aliyuncs.com/data/upload/content/20190124/5c4967c876404.gif' },
         { imageUrl: 'https://miniyx.oss-cn-beijing.aliyuncs.com/data/upload/content/20190124/5c4967c876404.gif' }
-      ]
+      ],
+      category: [],
+      adBanner: '',
+      recommendGoods: [] 
     }
   },
   components: {
-
+    swiper,
+    swiperSlide,
+    floorComponent
   }
 }
 </script>
@@ -74,7 +130,40 @@ export default {
 .swiper-area{
   width:20rem;
   clear:both;
-  max-height 15rem;
+  // max-height 15rem;
   overflow hidden
+}
+.type-bar{
+  background-color: #fff;
+  margin:0 .3rem .3rem .3rem;
+  border-radius: .3rem;
+  font-size:14px;
+  display: flex;
+  flex-direction:row;
+  flex-wrap:nowrap;
+}
+.type-bar div{
+  padding: .3rem;
+  font-size: 12px;
+  text-align: center;
+}
+.recommend-area{
+  background-color: #fff;
+  margin-top: .3rem;
+}
+.recommend-title{
+  border-bottom:1px solid #eee;
+  font-size:14px;
+  padding:.2rem;
+  color:#e5017d;
+}
+.recommend-body{
+  border-bottom: 1px solid #eee;
+}
+.recommend-item{
+  width:99%;
+  border-right: 1px solid #eee;
+  font-size: 12px;
+  text-align: center;
 }
 </style>
